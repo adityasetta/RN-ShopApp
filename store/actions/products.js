78486c0e0bca57fe5b5f7a35445a9f0fb7077350
Aditya Parama Setta <aditya.psetta@bukitmakmur.com>
2021-08-14 16:54:58 +0700

    Add loading spinner and error handler

--- store/actions/products.js
+++ store/actions/products.js
@@ -8,28 +8,36 @@ export const SET_PRODUCTS = 'SET_PRODUCTS';
 export const fetchProducts = () => {
   return async dispatch => {
     // any async code you want!
-    const response = await fetch(
-      'https://react-native-shop-app-37c49-default-rtdb.asia-southeast1.firebasedatabase.app/products.json'
-    );
-
-    const resData = await response.json();
-    const loadedProducts = [];
-
-    for (const key in resData) {
-      loadedProducts.push(
-        new Product(
-          key,
-          'u1',
-          resData[key].title,
-          resData[key].imageUrl,
-          resData[key].description,
-          resData[key].price
-        )
+    try {
+      const response = await fetch(
+        'https://react-native-shop-app-37c49-default-rtdb.asia-southeast1.firebasedatabase.app/products.json'
       );
 
-    }
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
 
-    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+      const resData = await response.json();
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            'u1',
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (err) {
+      // send to custom analytics server
+      throw err;
+    }
   };
 };
 
@@ -39,20 +47,22 @@ export const deleteProduct = productId => {
 
 export const createProduct = (title, description, imageUrl, price) => {
   return async dispatch => {
-        // any async code you want!
-        const response = await fetch(
-          'https://react-native-shop-app-37c49-default-rtdb.asia-southeast1.firebasedatabase.app/products.json', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            title,
-            description,
-            imageUrl,
-            price
-          })
-        });
+    // any async code you want!
+    const response = await fetch(
+      'https://react-native-shop-app-37c49-default-rtdb.asia-southeast1.firebasedatabase.app/products.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+          price
+        })
+      }
+    );
 
     const resData = await response.json();
 
