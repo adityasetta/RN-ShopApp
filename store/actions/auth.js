4564278394ec9778e6f2c4e49186e9f820e90b1d
Aditya Parama Setta <aditya.psetta@bukitmakmur.com>
2021-08-14 20:24:24 +0700

    Add Log In function

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,4 +1,5 @@
 export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
 
 export const signup = (email, password) => {
   return async dispatch => {
@@ -26,3 +27,30 @@ export const signup = (email, password) => {
     dispatch({ type: SIGNUP });
   };
 };
+
+export const login = (email, password) => {
+    return async dispatch => {
+      const response = await fetch(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDNXsl66pq-dg4vUZHYAbU9QXgPurlGisU',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            returnSecureToken: true
+          })
+        }
+      );
+  
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+  
+      const resData = await response.json();
+      console.log(resData);
+      dispatch({ type: LOGIN });
+    };
+  };
